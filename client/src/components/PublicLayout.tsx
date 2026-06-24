6e1c12c3e556ef64494eb8236cdd7a3ada501922
l6828833-ship-it <l6828833@gmail.com>
2026-06-24 20:50:32 +0100

    Update PublicLayout.tsx

--- client/src/components/PublicLayout.tsx
+++ client/src/components/PublicLayout.tsx
@@ -24,7 +24,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
             <img
               src="https://pub-5b34ab7e74be4b678343a2ff1c41d64c.r2.dev/iptvtop%20live%20logo.png"
               alt="IPTV TOP"
-              className="h-14 w-auto"
+              className="h-20 w-auto"
             />
           </Link>
           
